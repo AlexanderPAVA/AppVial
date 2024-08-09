b8eb0eabfc64b3c450f1e317b43b686b78ae016c
AlexP <113405853+AlexanderPAVA@users.noreply.github.com>
2024-08-09 11:38:43 -0500

    Update BorrarFotoUrl.jsx
    refactorizado

--- src/components/BorrarFotoUrl.jsx
+++ src/components/BorrarFotoUrl.jsx
@@ -1,26 +1,15 @@
 import RNFS from 'react-native-fs';
-export function BorrarFotoUrl(uriImg) {
-    const eliminar = ()=>{
-        if (uriImg) {
-        try {        
-            RNFS.readdir('file:///storage/emulated/0/Android/data/com.yoreporto/files/Pictures')
-            .then((files) => {
-              const jpgFiles = files.filter((file) => file.endsWith('.jpg'));
-              jpgFiles.forEach((file) => {
-                const filePath = `${'file:///storage/emulated/0/Android/data/com.yoreporto/files/Pictures'}/${file}`;
-                   RNFS.unlink(filePath)
-                  .then(() => {
-                  
-                  })
-                  .catch((err) => {
-                  });
-              });
-
-            })
-        } catch (error) { 
-        }
-      } else { 
-      }    
+export function BorrarFotoUrl() {
+    const eliminar = async()=>{
+      try {
+        const directoryPath = 'file:///storage/emulated/0/Android/data/com.yoreporto/files/Pictures';
+        const files = await RNFS.readdir(directoryPath);
+        const jpgFiles = files.filter(file => file.endsWith('.jpg'));
+        const filePaths = jpgFiles.map(file => `${directoryPath}/${file}`);
+        await Promise.all(filePaths.map(filePath => RNFS.unlink(filePath)));
+      } catch (error) {
+        console.error('Error al borrar fotos:', error);
+      }
     }
     eliminar();
 };
