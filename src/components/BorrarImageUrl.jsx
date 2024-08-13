3eff6ea54fa8c7b444329651c315319ca8fdb280
AlexP <113405853+AlexanderPAVA@users.noreply.github.com>
2024-08-13 12:58:26 -0500

    Update BorrarImageUrl.jsx
    refactorizado

--- src/components/BorrarImageUrl.jsx
+++ src/components/BorrarImageUrl.jsx
@@ -1,14 +1,12 @@
 import RNFS from 'react-native-fs';
-
 export function BorrarImageUrl(uriImg) {
-    const eliminar = async()=>{
-        if (uriImg) {
-        try {
-          await RNFS.unlink(uriImg);
-        } catch (error) {
-        }
-      } else {
-      }    
+  if (!uriImg) return;  // Salir si uriImg no está definido o es nulo
+  const eliminar = async () => {
+    try {
+      await RNFS.unlink(uriImg);
+    } catch (error) {
+      console.error('Error al borrar la imagen:', error);
     }
-    eliminar();
+  };
+  eliminar();
 }
